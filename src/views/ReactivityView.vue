72ecad7ca2413eeb68e53d96601503e7ec186cf4
rock's ezweb <alikerock@gmail.com>
2024-11-14 15:07:58 +0900

    first commit

---
+++ src/views/ReactivityView.vue
@@ -0,0 +1,26 @@
+<script setup>
+  import { ref } from 'vue'
+
+  // 상태(State)
+  const count = ref(0)
+
+  // 기능(Actions)
+  function increment() {
+    count.value++
+  }
+</script>
+<template>
+  <div class="content">
+    <h1>반응 기초(상태관리)</h1>
+    <div>{{ count }}</div>
+    <button @click="increment">Add Count</button>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .content {
+    min-height: 100vh;
+  }
+}
+</style>

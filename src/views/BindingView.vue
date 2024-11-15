9b1a7d6c438dc2185cb696f390e879ae8a6afb6b
rock's ezweb <alikerock@gmail.com>
2024-11-15 12:59:34 +0900

    클래스와 스타일 바인딩 추가

---
+++ src/views/BindingView.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, reactive, computed } from 'vue';
+
+  const isActive = ref(true)
+  const hasError = ref(false)
+
+  // const classObj =reactive({
+  //   active:true,
+  //   error:false
+  // });
+  const myfunc = computed(()=>{
+    return {
+      active:isActive.value,
+      error:hasError.value
+    }
+  })
+</script>
+
+<template>
+  <div class="content">
+    <h1>클래스와 스타일 바인딩</h1>
+    <div class="static" :class="myfunc">content</div>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .content {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>

39d8490e33e41fb13296af0bb70450f8eced2dde
rock's ezweb <alikerock@gmail.com>
2024-11-26 11:38:41 +0900

    감시자 이미지 출력

--- src/views/WatchView.vue
+++ src/views/WatchView.vue
@@ -4,13 +4,17 @@ import { ref, watch } from 'vue'
 const question = ref('')
 const answer = ref('질문에는 보통 물음표가 포함됩니다. ;-)')
 const loading = ref(false)
+const answerImg = ref('');
+
 watch(question, async(newQestion, oldQuestion)=>{
   if(newQestion.includes('?')){
     loading.value=true;
     answer.value = '생각중...';
     try{
       const res = await fetch('https://yesno.wtf/api')
-      answer.value =  (await res.json()).answer === 'yes' ?'네':'아니오'
+      const result = await res.json();
+      answer.value =  result.answer === 'yes' ?'네':'아니오'
+      answerImg.value = result.image
     } catch (error){
       answer.value =  '에러!' +error
     }
@@ -22,8 +26,11 @@ watch(question, async(newQestion, oldQuestion)=>{
   <div class="content">
     <h1>감시자</h1>
     <!-- <input type="text" @input="event=>question=event.target.value"> -->
-    <input type="text" v-model="question">
+    <input type="text" v-model="question" :disabled="loading" />
     <p>{{ answer }}</p>
+    <p v-if="answerImg">
+      <img :src="answerImg" alt="">
+    </p>
   </div>
 </template>
 
